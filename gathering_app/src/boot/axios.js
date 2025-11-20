3895f44755d03c09c1b73a11f7997396a592c93e
Gabriel Vianna <gabriel.viannasr@gmail.com>
2025-11-20 14:57:17 -0300

    Axios: Add interceptors

--- gathering_app/src/boot/axios.js
+++ gathering_app/src/boot/axios.js
@@ -1,5 +1,6 @@
 import { defineBoot } from '#q-app/wrappers'
 import axios from 'axios'
+import { Notify } from 'quasar'
 
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
@@ -8,6 +9,57 @@ import axios from 'axios'
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({ baseURL: process.env.QUASAR_APP_GATHERING_SERVICE_HOST })
+
+api.interceptors.request.use(
+  config => {
+    // exemplo: adicionar token
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  error => Promise.reject(error)
+)
+
+// api.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response) {
+//       console.error('Erro da API:', error.response.data)
+//     }
+//     return Promise.reject(error)
+//   }
+// )
+
+api.interceptors.response.use(
+  response => {
+    const method = response.config.method?.toLowerCase()
+
+    if (response.status === 200 && (method === 'post' || method === 'put')) {
+      Notify.create({
+        type: 'positive',
+        message: 'Sucesso!'
+      })
+    }
+
+    return response
+  },
+  error => {
+    const msg =
+      error.response?.data?.message ||
+      error.response?.data ||
+      error.message ||
+      'Erro ao processar a requisição'
+
+    Notify.create({
+      type: 'negative',
+      message: `Erro: ${msg}`
+    })
+
+    return Promise.reject(error)
+  }
+)
 
 export default defineBoot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
