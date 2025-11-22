59eb330d2ef9c8dfd678a43258cec9a0bcd4073f
Gabriel Vianna <gabriel.viannasr@gmail.com>
2025-11-22 01:55:18 -0300

    refactor(useRankNavigator): Optimize navigator

--- gathering_app/src/composables/navigation/useRankNavigator.js
+++ gathering_app/src/composables/navigation/useRankNavigator.js
@@ -3,20 +3,15 @@ import { useRouter } from 'vue-router'
 export function useRankNavigator() {
   const router = useRouter()
 
-  function goToRankEvents() {
-    router.push({ name: 'rank-eventos' })
-  }
+  const goToRankEvents = () => router.push({ name: 'rank-eventos' })
 
-  function goToRankEvent(idEvent) {
-    router.push({ name: 'rank', params: { idEvent } })
-  }
+  const goToRankEvent = idEvent => router.push({ name: 'rank', params: { idEvent } })
 
-  function goToRankPlayer(idEvent, idPlayer) {
+  const goToRankPlayer = (idEvent, idPlayer) =>
     router.push({
       name: 'rank-player',
       params: { idEvent, idPlayer }
     })
-  }
 
   return {
     goToRankEvents,
