04a708253a3326c54e5a9487a97416ab98fcc593
Gabriel Vianna <gabriel.viannasr@gmail.com>
2025-11-26 13:48:52 -0300

    refactor(FormatListPage): Use pinia store

--- src/pages/home/formats/FormatListPage.vue
+++ src/pages/home/formats/FormatListPage.vue
@@ -3,17 +3,12 @@
     <!-- FILTROS -->
     <div class="q-pa-md">
       <q-card class="q-pa-md form-card">
-        <!-- Titile -->
         <div class="form-section-title">Filtros</div>
 
-        <!-- Nome -->
         <GlobalInput label="Nome" v-model="filters.name" debounce="300">
-          <template #prepend>
-            <q-icon name="search" />
-          </template>
+          <template #prepend><q-icon name="search" /></template>
         </GlobalInput>
 
-        <!-- Botão Adicionar -->
         <div class="q-mt-sm">
           <q-btn class="add-btn full-width" no-caps rounded unelevated @click="onAdd">
             <q-icon name="add" class="q-mr-sm" />
@@ -27,24 +22,22 @@
     <div class="q-pa-md q-gutter-md">
       <q-card
         v-for="item in filteredFormats"
-        :key="item.name"
+        :key="item.id"
         class="list-card q-pa-sm"
         clickable
         @click="openFormat(item)"
       >
         <div class="row items-center no-wrap">
           <div class="icon-circle q-mr-md">
-            <q-icon :name="item.icon" color="white" size="24px" />
+            <q-icon :name="resolveType(item.idFormatType)?.icon" color="white" size="24px" />
           </div>
 
           <div class="col">
             <div class="text-subtitle2 text-bold">{{ item.name }}</div>
-            <div class="text-caption text-grey">{{ item.hp }} pontos de vida</div>
+            <div class="text-caption text-grey">{{ item.lifeCount }} pontos de vida</div>
           </div>
 
-          <div>
-            <q-icon name="chevron_right" />
-          </div>
+          <q-icon name="chevron_right" />
         </div>
       </q-card>
 
@@ -59,39 +52,33 @@
 <script setup>
   import GlobalInput from 'src/components/ui/GlobalInput.vue'
   import { ref, computed } from 'vue'
-  import { useConfraStore } from 'src/stores/confra'
   import { useFormatNavigator } from 'src/composables/navigation'
+  import { useFormatStore } from 'src/stores/format'
+  import { useFormatTypeStore } from 'src/stores/formatType'
 
   const { goToNewFormat, goToEditFormat } = useFormatNavigator()
 
-  // eslint-disable-next-line no-unused-vars
-  const confra = useConfraStore()
-  const filters = ref({ name: '' })
+  const formatStore = useFormatStore()
+  const typeStore = useFormatTypeStore()
 
-  const formats = [
-    { id: 1, name: 'Commander', hp: 40, icon: 'style' },
-    { id: 2, name: 'Conquest', hp: 30, icon: 'style' },
-    { id: 3, name: 'Tiny Leader', hp: 30, icon: 'style' },
-    { id: 4, name: 'Detetive', hp: 0, icon: 'casino' },
-    { id: 5, name: 'Ludo', hp: 0, icon: 'casino' },
-    { id: 6, name: 'Adedonha ou Stop', hp: 0, icon: 'assignment' }
-  ]
+  const filters = ref({ name: '' })
 
   const page = ref(1)
   const maxPages = 3
 
   const filteredFormats = computed(() =>
-    formats.filter(f => f.name.toLowerCase().includes(filters.value.name.toLowerCase()))
+    formatStore.formats.filter(f => f.name.toLowerCase().includes(filters.value.name.toLowerCase()))
   )
 
+  function resolveType(typeId) {
+    return typeStore.getType(typeId)
+  }
+
   function onAdd() {
-    // abrir modal / rota de criar formato
-    console.log('Adicionar formato')
     goToNewFormat()
   }
 
   function openFormat(item) {
-    console.log('Abrir formato', item)
     goToEditFormat(item.id)
   }
 </script>
