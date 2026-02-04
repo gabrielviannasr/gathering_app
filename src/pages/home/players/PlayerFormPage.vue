add802d83edc5f357fad84d61c559b1bb5275b76
Gabriel Vianna <gabriel.viannasr@gmail.com>
2026-02-04 14:50:37 -0300

    PlayerFormPage: Use api instead of mockup data

--- src/pages/home/players/PlayerFormPage.vue
+++ src/pages/home/players/PlayerFormPage.vue
@@ -1,50 +1,82 @@
 <template>
   <q-page class="page-bg">
-    <div class="q-pa-md">
-      <q-card class="q-pa-md form-card">
-        <!-- Title -->
-        <div class="form-section-title">Jogador</div>
+    <q-form @submit.prevent="save">
+      <div class="q-pa-md">
+        <q-card class="q-pa-md form-card">
+          <!-- Title -->
+          <div class="form-section-title">Jogador</div>
 
-        <!-- Nome -->
-        <GlobalInput label="Nome" v-model="form.name" placeholder="Digite o nome do jogador" />
-      </q-card>
+          <!-- Nome -->
+          <GlobalInput label="Nome" v-model="form.name" placeholder="Digite o nome do jogador" />
+        </q-card>
 
-      <!-- Botões -->
-      <div class="row q-col-gutter-md q-mt-md">
-        <div class="col">
-          <q-btn outline color="grey-8" no-caps rounded class="full-width" @click="cancel">
-            Cancelar
-          </q-btn>
-        </div>
+        <!-- Botões -->
+        <div class="row q-col-gutter-md q-mt-md">
+          <div class="col">
+            <q-btn outline color="grey-8" no-caps rounded class="full-width" @click="cancel">
+              Cancelar
+            </q-btn>
+          </div>
 
-        <div class="col">
-          <q-btn class="add-btn full-width" no-caps rounded unelevated @click="save">
-            Salvar
-          </q-btn>
+          <div class="col">
+            <q-btn type="submit" class="add-btn full-width" no-caps rounded unelevated>
+              Salvar
+            </q-btn>
+          </div>
         </div>
       </div>
-    </div>
+    </q-form>
   </q-page>
 </template>
 
 <script setup>
   import GlobalInput from 'src/components/ui/GlobalInput.vue'
-  import { ref } from 'vue'
-  import { useRouter } from 'vue-router'
+  import { onMounted, ref } from 'vue'
+  import { useRoute, useRouter } from 'vue-router'
+  import { usePlayerStore } from 'src/stores/player'
 
+  const playerStore = usePlayerStore()
+
+  const route = useRoute()
   const router = useRouter()
 
-  // formulário
+  const id = route.params.id
+  const isEdit = !!id
+
   const form = ref({
     name: ''
+  })
+
+  async function save() {
+    try {
+      if (isEdit) {
+        await playerStore.updatePlayer(id, form.value)
+      } else {
+        await playerStore.createPlayer(form.value)
+      }
+
+      router.back()
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  onMounted(async () => {
+    if (isEdit) {
+      const player = await playerStore.getPlayer(id)
+
+      form.value = {
+        name: player.name
+      }
+    }
   })
 
   function cancel() {
     router.back()
   }
 
-  function save() {
-    console.log('Salvar jogador:', form.value)
-    router.back()
-  }
+  // function save() {
+  //   console.log('Salvar jogador:', form.value)
+  //   router.back()
+  // }
 </script>
--- src/stores/player.js
+++ src/stores/player.js
@@ -1,24 +1,57 @@
 import { defineStore } from 'pinia'
+import { api } from 'boot/axios'
 import { ref } from 'vue'
 
 export const usePlayerStore = defineStore('player', () => {
+  const player = ref(null)
   const players = ref([
-    { id: 1, name: 'Anderson Dias' },
-    { id: 2, name: 'Arthur Leal' },
-    { id: 3, name: 'Cindomar Ferreira' },
-    { id: 4, name: 'Gabriel Vianna' },
-    { id: 5, name: 'Jean Benevides' },
-    { id: 6, name: 'Jhonny Dias' },
-    { id: 7, name: 'Tobias Souza' },
-    { id: 8, name: 'Valmir Vicente' }
+    // { id: 1, name: 'Anderson Dias' },
+    // { id: 2, name: 'Arthur Leal' },
+    // { id: 3, name: 'Cindomar Ferreira' },
+    // { id: 4, name: 'Gabriel Vianna' },
+    // { id: 5, name: 'Jean Benevides' },
+    // { id: 6, name: 'Jhonny Dias' },
+    // { id: 7, name: 'Tobias Souza' },
+    // { id: 8, name: 'Valmir Vicente' }
   ])
 
-  function getPlayer(id) {
-    return players.value.find(p => p.id === id)
+  async function getPlayers(params) {
+    try {
+      const res = await api.get(`/player`, { params })
+      this.players = res.data
+      return this.players
+    } catch (err) {
+      return err
+    }
+  }
+
+  async function getPlayer(id) {
+    // return players.value.find(p => p.id === id)
+    try {
+      const res = await api.get(`/player/${id}`)
+      this.player = res.data
+      return this.player
+    } catch (err) {
+      return err
+    }
+  }
+
+  async function createPlayer(data) {
+    const res = await api.post('/player', data)
+    return res.data
+  }
+
+  async function updatePlayer(id, data) {
+    const res = await api.put(`/player/${id}`, data)
+    return res.data
   }
 
   return {
+    player,
     players,
-    getPlayer
+    getPlayer,
+    getPlayers,
+    createPlayer,
+    updatePlayer
   }
 })
