044a955216678433455de2edb008ac6e96b2ea69
Gabriel Vianna <gabriel.viannasr@gmail.com>
2026-02-07 20:37:11 -0300

    RoundEventList: Load formats and events

--- src/pages/home/rounds/RoundEventListPage.vue
+++ src/pages/home/rounds/RoundEventListPage.vue
@@ -5,10 +5,29 @@
 <script setup>
   import EventListBase from 'src/components/events/EventListBase.vue'
   import { useRoundNavigator } from 'src/composables/navigation'
+  import { onMounted } from 'vue'
+  import { useConfraStore } from 'src/stores/confra'
+  import { useEventStore } from 'src/stores/event'
+  import { useFormatStore } from 'src/stores/format'
 
   const { goToRounds } = useRoundNavigator()
 
+  const confraStore = useConfraStore()
+  const eventStore = useEventStore()
+  const formatStore = useFormatStore()
+
   function openRounds(item) {
     goToRounds(item.id)
+  }
+
+  // carregar ao abrir
+  onMounted(load)
+
+  async function load() {
+    await formatStore.getFormats()
+    // se tiver confra selecionada, carrega os eventos dela
+    if (confraStore.selectedConfra?.id) {
+      await eventStore.getEvents({ idGathering: confraStore.selectedConfra?.id })
+    }
   }
 </script>
