8ba2a62947a816af551a811987bfa0f60303d819
Gabriel Vianna <gabriel.viannasr@gmail.com>
2026-07-19 16:22:17 -0300

    feat(reports): integrate ConfraWinratePage with dashboard API

--- src/pages/reports/winrate/ConfraWinratePage.vue
+++ src/pages/reports/winrate/ConfraWinratePage.vue
@@ -3,42 +3,56 @@
   <q-page class="page-bg">
     <!-- Header da Confra -->
     <div class="q-pa-md">
-      <ConfraHeaderCard :confraSummary="confraSummary" />
+      <ConfraHeaderCard :confraSummary="confraSummary" v-if="confraSummary" />
     </div>
 
     <!-- Gráfico de Winrate -->
     <div class="q-pa-md">
-      <WinrateChartCard :winrateData="winrateList" />
+      <WinrateChartCard :winrateData="winrateData" v-if="winrateData" />
     </div>
   </q-page>
 </template>
 
 <script setup>
-  import { computed } from 'vue'
-  import { useRoute } from 'vue-router'
-
   import ConfraHeaderCard from 'src/components/confras/ConfraHeaderCard.vue'
   import WinrateChartCard from 'src/components/reports/WinrateChartCard.vue'
 
-  import { useConfraStore } from 'src/stores/confra'
+  import { computed, onMounted } from 'vue'
+  import { useRoute } from 'vue-router'
+
   import { useDashboardStore } from 'src/stores/dashboard'
 
-  /* ROUTE */
+  /* NAVIGATION */
   const route = useRoute()
   const idGathering = Number(route.params.id)
 
   /* STORES */
-  const confraStore = useConfraStore()
   const dashboardStore = useDashboardStore()
 
-  /* CONFRA HEADER */
-  const confraSummary = computed(() => confraStore.getConfraSummary(idGathering))
+  /* DATA */
+  const confraSummary = computed(() => dashboardStore.confraSummary)
 
-  /* WINRATE DATA */
-  const winrateList = computed(() =>
-    dashboardStore.getConfraResults(idGathering).map(r => ({
-      playerName: r.playerName,
+  const winrateData = computed(() =>
+    dashboardStore.confraResults.map(r => ({
+      playerName: r.player.name,
       winrate: r.rounds > 0 ? Number(((r.wins / r.rounds) * 100).toFixed(2)) : 0
     }))
   )
+
+  // const winrateData = computed(() =>
+  //   dashboardStore.getConfraResults(idGathering).map(r => ({
+  //     playerName: r.playerName,
+  //     winrate: r.rounds > 0 ? Number(((r.wins / r.rounds) * 100).toFixed(2)) : 0
+  //   }))
+  // )
+
+  /* ---------------- LOAD ---------------- */
+  onMounted(async () => {
+    await load()
+  })
+
+  async function load() {
+    await dashboardStore.getConfraSummary(idGathering)
+    await dashboardStore.getConfraResults(idGathering)
+  }
 </script>
