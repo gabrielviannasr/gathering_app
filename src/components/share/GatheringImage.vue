6b3fd7332a8f36b305edd0abb1112108b6632b52
Gabriel Vianna <gabriel.viannasr@gmail.com>
2026-08-11 23:22:17 -0300

    feat(share): add gathering image

---
+++ src/components/share/GatheringImage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page-bg">
+    <!-- CARD DA CONFRA -->
+    <div class="q-pa-md">
+      <GatheringCard :gathering="gathering" :showArrow="false" />
+    </div>
+
+    <!-- CARD DOS POTES -->
+    <div class="q-px-md q-pb-md">
+      <PotSummaryCard :pots="pots" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import GatheringCard from 'src/components/gatherings/GatheringCard.vue'
+  import PotSummaryCard from 'src/components/pots/PotSummaryCard.vue'
+
+  defineProps({
+    pots: {
+      type: Object,
+      required: true
+    },
+    gathering: {
+      type: Object,
+      required: true
+    }
+  })
+</script>
--- src/pages/reports/pots/GatheringPotsPage.vue
+++ src/pages/reports/pots/GatheringPotsPage.vue
@@ -31,6 +31,25 @@
         <PotSummaryCard :pots="gatheringSummary" />
       </div>
 
+      <div style="position: absolute; left: -10000px; top: 0; width: 100%">
+        <GatheringImage ref="gatheringRef" :gathering="gathering" :pots="gatheringSummary" />
+      </div>
+
+      <!-- BOTÕES DE AÇÃO -->
+      <div class="row q-col-gutter-sm q-pa-md">
+        <div class="col-12">
+          <q-btn
+            push
+            no-caps
+            rounded
+            label="Compartilhar Confra"
+            icon="share"
+            class="add-btn full-width"
+            @click="shareGathering"
+          />
+        </div>
+      </div>
+
       <!-- LISTA -->
       <div class="q-pa-md q-gutter-md" v-if="events.length > 0">
         <EventCard
@@ -47,12 +66,13 @@
 
 <script setup>
   /* VUE */
-  import { computed, onMounted } from 'vue'
+  import { computed, onMounted, ref } from 'vue'
   import { useRoute, useRouter } from 'vue-router'
 
   /* STORES */
   import { useDashboardStore } from 'src/stores/dashboard'
   import { useEventStore } from 'src/stores/event'
+  import { useGatheringStore } from 'src/stores/gathering'
 
   /* COMPOSABLES */
   import { useRankNavigator } from 'src/composables/navigation'
@@ -60,8 +80,12 @@
   /* COMPONENTS */
   import EmptyStateCard from 'src/components/ui/EmptyStateCard.vue'
   import EventCard from 'src/components/events/EventCard.vue'
+  import GatheringImage from 'src/components/share/GatheringImage.vue'
   import GatheringSummaryCard from 'src/components/gatherings/GatheringSummaryCard.vue'
   import PotSummaryCard from 'src/components/pots/PotSummaryCard.vue'
+
+  /* UTILITIES */
+  import { dataUrlToFile, generateImage, shareImage } from 'src/utils'
 
   /* COMPOSABLES */
   const { goToRankEvent } = useRankNavigator()
@@ -76,10 +100,14 @@
   /* STORES */
   const dashboardStore = useDashboardStore()
   const eventStore = useEventStore()
+  const gatheringStore = useGatheringStore()
 
   /* COMPUTED */
   const gatheringSummary = computed(() => dashboardStore.gatheringSummary)
   const events = computed(() => eventStore.events ?? [])
+  const gathering = computed(() => gatheringStore.gatheringSelected)
+
+  const gatheringRef = ref(null)
 
   /* LIFECYCLE */
   onMounted(async () => {
@@ -99,5 +127,17 @@
 
   function openEvent(event) {
     goToRankEvent(event.id)
+  }
+
+  async function shareGathering() {
+    const dataUrl = await generateImage(gatheringRef)
+
+    const filename = `confra-${gathering.value.id}.png`
+
+    const title = 'Confra'
+
+    const file = dataUrlToFile(dataUrl, filename)
+
+    await shareImage(file, title)
   }
 </script>
